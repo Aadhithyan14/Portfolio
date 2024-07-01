9dff7f866f35e900587b422400b1c0584087c5e9
Adithyan S <osadithy@amazon.com>
2024-07-01 21:12:26 +0530

    Commit message

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+// import pdf from "../../Assets/../Assets/Adithyan.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const pdf = "https://github.com/Aadhithyan14/Resume/blob/main/Adithyan.pdf";
+
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -18,6 +21,7 @@ function ResumeNew() {
   return (
     <div>
       <Container fluid className="resume-section">
+
         <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
